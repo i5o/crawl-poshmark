1ad9b2c3391dfdec218782eb3192de378dd614d4
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-04-01 23:58:55 -0300

    folders

---
+++ poshmark/data/links.py
@@ -0,0 +1,208 @@
+links = \
+{
+ "Accessories":
+    [
+        ("Belts", "https://poshmark.com/category/Women-Accessories-Belts"),
+        ("Glasses", "https://poshmark.com/category/Women-Accessories-Glasses"),
+        ("Gloves & Mittens", "https://poshmark.com/category/Women-Accessories-Gloves_&_Mittens"),
+        ("Hair Accessories", "https://poshmark.com/category/Women-Accessories-Hair_Accessories"),
+        ("Hats", "https://poshmark.com/category/Women-Accessories-Hats"),
+        ("Hosiery & Socks", "https://poshmark.com/category/Women-Accessories-Hosiery_&_Socks"),
+        ("Key & Card Holders", "https://poshmark.com/category/Women-Accessories-Key_&_Card_Holders"),
+        ("Laptop Cases", "https://poshmark.com/category/Women-Accessories-Laptop_Cases"),
+        ("Phone Cases", "https://poshmark.com/category/Women-Accessories-Phone_Cases"),
+        ("Scarves & Wraps", "https://poshmark.com/category/Women-Accessories-Scarves_&_Wraps"),
+        ("Sunglasses", "https://poshmark.com/category/Women-Accessories-Sunglasses"),
+        ("Tablet Cases", "https://poshmark.com/category/Women-Accessories-Tablet_Cases"),
+        ("Umbrellas", "https://poshmark.com/category/Women-Accessories-Umbrellas"),
+        ("Watches", "https://poshmark.com/category/Women-Accessories-Watches")
+    ],
+
+ "Bags":
+    [
+        ("Baby Bags", "https://poshmark.com/category/Women-Bags-Baby_Bags"),
+        ("Backpacks", "https://poshmark.com/category/Women-Bags-Backpacks"),
+        ("Cosmetic Bags & Cases", "https://poshmark.com/category/Women-Bags-Cosmetic_Bags_&_Cases"),
+        ("Crossbody Bags", "https://poshmark.com/category/Women-Bags-Crossbody_Bags"),
+        ("Hobos", "https://poshmark.com/category/Women-Bags-Hobos"),
+        ("Laptop Bags", "https://poshmark.com/category/Women-Bags-Laptop_Bags"),
+        ("Mini Bags", "https://poshmark.com/category/Women-Bags-Mini_Bags"),
+        ("Satchels", "https://poshmark.com/category/Women-Bags-Satchels"),
+        ("Shoulder Bags", "https://poshmark.com/category/Women-Bags-Shoulder_Bags"),
+        ("Totes", "https://poshmark.com/category/Women-Bags-Totes"),
+        ("Travel Bags", "https://poshmark.com/category/Women-Bags-Travel_Bags"),
+        ("Clutches & Wristlets", "https://poshmark.com/category/Women-Bags-Clutches_&_Wristlets"),
+        ("Wallets", "https://poshmark.com/category/Women-Bags-Wallets")
+    ],
+
+ "Dresses":
+    [
+        ("Mini", "https://poshmark.com/category/Women-Dresses-Mini"),
+        ("Midi", "https://poshmark.com/category/Women-Dresses-Midi"),
+        ("Maxi", "https://poshmark.com/category/Women-Dresses-Maxi"),
+        ("High Low", "https://poshmark.com/category/Women-Dresses-High_Low"),
+        ("Asymmetrical", "https://poshmark.com/category/Women-Dresses-Asymmetrical"),
+        ("Long Sleeve", "https://poshmark.com/category/Women-Dresses-Long_Sleeve"),
+        ("One Shoulder", "https://poshmark.com/category/Women-Dresses-One_Shoulder"),
+        ("Strapless", "https://poshmark.com/category/Women-Dresses-Strapless"),
+        ("Backless", "https://poshmark.com/category/Women-Dresses-Backless"),
+        ("Prom", "https://poshmark.com/category/Women-Dresses-Prom"),
+        ("Wedding", "https://poshmark.com/category/Women-Dresses-Wedding")
+    ],
+
+ "Intimates & Sleepwear":
+    [
+        ("Bras", "https://poshmark.com/category/Women-Intimates_&_Sleepwear-Bras"),
+        ("Bandeaus", "https://poshmark.com/category/Women-Intimates_&_Sleepwear-Bandeaus"),
+        ("Panties", "https://poshmark.com/category/Women-Intimates_&_Sleepwear-Panties"),
+        ("Shapewear", "https://poshmark.com/category/Women-Intimates_&_Sleepwear-Shapewear"),
+        ("Pajamas", "https://poshmark.com/category/Women-Intimates_&_Sleepwear-Pajamas"),
+        ("Robes", "https://poshmark.com/category/Women-Intimates_&_Sleepwear-Robes"),
+        ("Chemises & Slips", "https://poshmark.com/category/Women-Intimates_&_Sleepwear-Chemises_&_Slips")
+    ],
+
+ "Jackets & Coats":
+    [
+        ("Blazers", "https://poshmark.com/category/Women-Jackets_&_Coats-Blazers"),
+        ("Capes", "https://poshmark.com/category/Women-Jackets_&_Coats-Capes"),
+        ("Jean Jackets", "https://poshmark.com/category/Women-Jackets_&_Coats-Jean_Jackets"),
+        ("Pea Coats", "https://poshmark.com/category/Women-Jackets_&_Coats-Pea_Coats"),
+        ("Puffers", "https://poshmark.com/category/Women-Jackets_&_Coats-Puffers"),
+        ("Trench Coats", "https://poshmark.com/category/Women-Jackets_&_Coats-Trench_Coats"),
+        ("Utility Jackets", "https://poshmark.com/category/Women-Jackets_&_Coats-Utility_Jackets"),
+        ("Vests", "https://poshmark.com/category/Women-Jackets_&_Coats-Vests")
+    ],
+
+ "Jeans":
+    [
+        ("Ankle & Cropped", "https://poshmark.com/category/Women-Jeans-Ankle_&_Cropped"),
+        ("Boot Cut", "https://poshmark.com/category/Women-Jeans-Boot_Cut"),
+        ("Boyfriend", "https://poshmark.com/category/Women-Jeans-Boyfriend"),
+        ("Flare & Wide Leg", "https://poshmark.com/category/Women-Jeans-Flare_&_Wide_Leg"),
+        ("Overalls", "https://poshmark.com/category/Women-Jeans-Overalls"),
+        ("Skinny", "https://poshmark.com/category/Women-Jeans-Skinny"),
+        ("Straight Leg", "https://poshmark.com/category/Women-Jeans-Straight_Leg")
+    ],
+
+ "Jewelry":
+    [
+        ("Braceletes", "https://poshmark.com/category/Women-Jewelry-Bracelets"),
+        ("Earrings", "https://poshmark.com/category/Women-Jewelry-Earrings"),
+        ("Necklaces", "https://poshmark.com/category/Women-Jewelry-Necklaces"),
+        ("Rings", "https://poshmark.com/category/Women-Jewelry-Rings"),
+        ("Brooches", "https://poshmark.com/category/Women-Jewelry-Brooches")
+    ],
+
+ "Makeup":
+    [
+        ("Blush", "https://poshmark.com/category/Women-Makeup-Blush"),
+        ("Bronzer", "https://poshmark.com/category/Women-Makeup-Bronzer"),
+        ("Concealer", "https://poshmark.com/category/Women-Makeup-Concealer"),
+        ("Eye Primer", "https://poshmark.com/category/Women-Makeup-Eye_Primer"),
+        ("Eyebrow Filler", "https://poshmark.com/category/Women-Makeup-Eyebrow_Filler"),
+        ("Eyeliner", "https://poshmark.com/category/Women-Makeup-Eyeliner"),
+        ("Eyeshadow", "https://poshmark.com/category/Women-Makeup-Eyeshadow"),
+        ("Face Powder", "https://poshmark.com/category/Women-Makeup-Face_Powder"),
+        ("Face Primer", "https://poshmark.com/category/Women-Makeup-Face_Primer"),
+        ("False Eyelashes", "https://poshmark.com/category/Women-Makeup-False_Eyelashes"),
+        ("Foundation", "https://poshmark.com/category/Women-Makeup-Foundation"),
+        ("Lip Balm & Gloss", "https://poshmark.com/category/Women-Makeup-Foundation"),
+        ("Lip Lines", "https://poshmark.com/category/Women-Makeup-Lip_Liner"),
+        ("Lipstick", "https://poshmark.com/category/Women-Makeup-Lipstick"),
+        ("Luminizer", "https://poshmark.com/category/Women-Makeup-Luminizer"),
+        ("Mascara", "https://poshmark.com/category/Women-Makeup-Mascara"),
+        ("Brushes & Tools", "https://poshmark.com/category/Women-Makeup-Brushes_&_Tools")
+    ],
+
+ "Pants":
+    [
+        ("Ankle & Cropped", "https://poshmark.com/category/Women-Pants-Ankle_&_Cropped"),
+        ("Boot Cut & Flare", "https://poshmark.com/category/Women-Pants-Boot_Cut_&_Flare"),
+        ("Capris", "https://poshmark.com/category/Women-Pants-Boot_Cut_&_Flare"),
+        ("Track Pants & Joggers", "https://poshmark.com/category/Women-Pants-Track_Pants_&_Joggers"),
+        ("Jumpsuits & Rompers", "https://poshmark.com/category/Women-Pants-Jumpsuits_&_Rompers"),
+        ("Leggings", "https://poshmark.com/category/Women-Pants-Leggings"),
+        ("Skinny", "https://poshmark.com/category/Women-Pants-Skinny"),
+        ("Straight Leg", "https://poshmark.com/category/Women-Pants-Straight_Leg"),
+        ("Trousers", "https://poshmark.com/category/Women-Pants-Trousers"),
+        ("Wide Leg", "https://poshmark.com/category/Women-Pants-Wide_Leg")
+    ],
+
+ "Shoes":
+    [
+        ("Ankle Boots & Booties", "https://poshmark.com/category/Women-Shoes-Ankle_Boots_&_Booties"),
+        ("Winter & Rain Boots", "https://poshmark.com/category/Women-Shoes-Winter_&_Rain_Boots"),
+        ("Combat & Moto Boots", "https://poshmark.com/category/Women-Shoes-Combat_&_Moto_Boots"),
+        ("Over the Knee Boots", "https://poshmark.com/category/Women-Shoes-Over_the_Knee_Boots"),
+        ("Heeled Boots", "https://poshmark.com/category/Women-Shoes-Heeled_Boots"),
+        ("Lace Up Boots", "https://poshmark.com/category/Women-Shoes-Lace_Up_Boots"),
+        ("Athletic Shoes", "https://poshmark.com/category/Women-Shoes-Athletic_Shoes"),
+        ("Espadrilles", "https://poshmark.com/category/Women-Shoes-Espadrilles"),
+        ("Flats & Loafers", "https://poshmark.com/category/Women-Shoes-Flats_&_Loafers"),
+        ("Moccasins", "https://poshmark.com/category/Women-Shoes-Moccasins"),
+        ("Mules & Clogs", "https://poshmark.com/category/Women-Shoes-Mules_&_Clogs"),
+        ("Platforms", "https://poshmark.com/category/Women-Shoes-Platforms"),
+        ("Heels", "https://poshmark.com/category/Women-Shoes-Heels"),
+        ("Sandals", "https://poshmark.com/category/Women-Shoes-Sandals"),
+        ("Slippers", "https://poshmark.com/category/Women-Shoes-Slippers"),
+        ("Sneakers", "https://poshmark.com/category/Women-Shoes-Sneakers"),
+        ("Wedges", "https://poshmark.com/category/Women-Shoes-Wedges")
+    ],
+
+ "Shorts":
+    [
+        ("Skorts", "https://poshmark.com/category/Women-Shorts-Skorts"),
+        ("Jean Shorts", "https://poshmark.com/category/Women-Shorts-Jean_Shorts"),
+        ("Bermudas", "https://poshmark.com/category/Women-Shorts-Bermudas"),
+        ("Cargos", "https://poshmark.com/category/Women-Shorts-Cargos")
+    ],
+
+ "Skirts":
+    [
+        ("Mini", "https://poshmark.com/category/Women-Skirts-Mini"),
+        ("Midi", "https://poshmark.com/category/Women-Skirts-Midi"),
+        ("Maxi", "https://poshmark.com/category/Women-Skirts-Maxi"),
+        ("High Low", "https://poshmark.com/category/Women-Skirts-High_Low"),
+        ("Asymmetrical", "https://poshmark.com/category/Women-Skirts-Asymmetrical"),
+        ("A-Line or Full", "https://poshmark.com/category/Women-Skirts-A_Line_or_Full"),
+        ("Circle & Skater", "https://poshmark.com/category/Women-Skirts-Circle_&_Skater"),
+        ("Pencil", "https://poshmark.com/category/Women-Skirts-Pencil"),
+        ("Skirt Sets", "https://poshmark.com/category/Women-Skirts-Skirt_Sets")
+    ],
+
+ "Sweaters":
+    [
+        ("Cardigans", "https://poshmark.com/category/Women-Sweaters-Cardigans"),
+        ("Crew & Scoop Necks", "https://poshmark.com/category/Women-Sweaters-Crew_&_Scoop_Necks"),
+        ("Shrugs & Ponchos", "https://poshmark.com/category/Women-Sweaters-Shrugs_&_Ponchos"),
+        ("Cowl & Turtlenecks", "https://poshmark.com/category/Women-Sweaters-Cowl_&_Turtlenecks"),
+        ("V-Necks", "https://poshmark.com/category/Women-Sweaters-V_Necks")
+    ],
+
+ "Swim":
+    [
+        ("Bikinis", "https://poshmark.com/category/Women-Swim-Bikinis"),
+        ("One Pieces", "https://poshmark.com/category/Women-Swim-One_Pieces"),
+        ("Coverups", "https://poshmark.com/category/Women-Swim-Coverups"),
+        ("Sarongs", "https://poshmark.com/category/Women-Swim-Sarongs")
+    ],
+
+ "Tops":
+    [
+        ("Blouses", "https://poshmark.com/category/Women-Tops-Blouses"),
+        ("Button Down Shirts", "https://poshmark.com/category/Women-Tops-Button_Down_Shirts"),
+        ("Camisoles", "https://poshmark.com/category/Women-Tops-Camisoles"),
+        ("Crop Tops", "https://poshmark.com/category/Women-Tops-Crop_Tops"),
+        ("Muscle Tees", "https://poshmark.com/category/Women-Tops-Muscle_Tees"),
+        ("Sweatshirts & Hoodies", "https://poshmark.com/category/Women-Tops-Sweatshirts_&_Hoodies"),
+        ("Tank Tops", "https://poshmark.com/category/Women-Tops-Tank_Tops"),
+        ("Tees - Long Sleeve", "https://poshmark.com/category/Women-Tops-Tees___Long_Sleeve"),
+        ("Tees - Short Sleeve", "https://poshmark.com/category/Women-Tops-Tees___Short_Sleeve"),
+        ("Tunics", "https://poshmark.com/category/Women-Tops-Tunics")
+    ],
+
+ "Other":
+    [
+        ("Other", "https://poshmark.com/category/Women-Other")
+    ]
+}
